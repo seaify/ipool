5e47b29dfd3837f41381acd827cdc3ec234a08f1
chuck.lei <dilin.life@gmail.com>
2015-05-22 06:01:15 -0400

    update

--- proxy-frontend/gulp/tasks/uglify.js
+++ proxy-frontend/gulp/tasks/uglify.js
@@ -1,7 +1,7 @@
 var uglify = require('gulp-uglify');
 var gulp = require('gulp');
 
-gulp.task('compress', function() {
+gulp.task('compress', ['browserify'], function() {
   return gulp.src('dist/js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dist/js/'));
