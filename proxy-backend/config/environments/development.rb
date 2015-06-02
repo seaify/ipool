8d9451497e61e5fe7bfa56efb97a0639a0f59165
chuck.lei <dilin.life@gmail.com>
2015-06-02 04:27:42 -0400

    fix smtp bug

--- proxy-backend/config/environments/development.rb
+++ proxy-backend/config/environments/development.rb
@@ -14,14 +14,16 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 8102}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'noreply.test.com',
-    user_name:            ENV['SEND_EMAIL'],
-    password:             ENV['SEND_EMAIL_PASSWD'],
+    domain:               'gmail.com',
+    user_name:          "norelpy.test@gmail.com",
+    password:            "lsm12345",
     authentication:       'plain',
     enable_starttls_auto: true
   }
